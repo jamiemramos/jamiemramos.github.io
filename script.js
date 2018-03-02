02d7ec52d2d4a8d236973b784455f533cc8f3835
Jamie Ramos <jmramos@live.unc.edu>
2018-03-02 21:33:35 +0000

    assignment 4

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var x = document.getElementById("gmail");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else 
+        x.style.display = "none";
+    }
